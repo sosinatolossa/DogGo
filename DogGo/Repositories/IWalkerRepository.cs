d17034b315724128371e6409eeaac4bfe1fe16db
sosinatolossa <sosinatolossa16@gmail.com>
2021-03-25 13:07:04 -0500

    initial commit

---
+++ DogGo/Repositories/IWalkerRepository.cs
@@ -0,0 +1,12 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public interface IWalkerRepository
+    {
+        List<Walker> GetAllWalkers(); //define a list type walker GetAllWalkers() method
+        Walker GetWalkerById(int id); //define a Walker type GetWalkerById() method that takes an id
+    }
+}
