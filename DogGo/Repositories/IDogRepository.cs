52b593770ae0349976bd3d5b1e210329823cd6a3
sosinatolossa <sosinatolossa16@gmail.com>
2021-03-29 23:18:51 -0500

    started on dog repo

---
+++ DogGo/Repositories/IDogRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Repositories
+{
+    public class IDogRepository
+    {
+        //List<Dog> GetDogsByOwnerId(int ownerId);
+
+    }
+}
